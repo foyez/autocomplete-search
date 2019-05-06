7110d14e32cd902f81529e8b04e76006b9603f60
foyezar <foyezar@yahoo.com>
2019-05-07 00:52:43 +0600

    show results for minimum 2 letters & show first 5 results

--- js/main.js
+++ js/main.js
@@ -5,24 +5,28 @@ const searchStates = async (searchText) => {
 	const res = await fetch('../data/states.json');
 	const states = await res.json();
 
-	// Get matches to current text input
-	let matches = states.filter((state) => {
-		const regex = new RegExp(`^${searchText}`, 'gi');
-		return state.name.match(regex) || state.abbr.match(regex);
-	});
+	if (searchText.length >= 2) {
+		// Get matches to current text input
+		let matches = states.filter((state) => {
+			const regex = new RegExp(`^${searchText}`, 'gi');
+			return state.name.match(regex) || state.abbr.match(regex);
+		});
 
-	if (searchText.length === 0) {
-		matches = [];
-		matchList.innerHTML = '';
+		console.log(matches);
+		if (searchText.length === 0) {
+			matches = [];
+			matchList.innerHTML = '';
+		}
+
+		showSuggestions(matches);
 	}
-
-	showSuggestions(matches);
 };
 
 // Show results
 const showSuggestions = (matches) => {
 	if (matches.length > 0) {
 		const html = matches
+			.slice(0, 5)
 			.map(
 				(match) => `
 			<div class="card card-body mb-1">
